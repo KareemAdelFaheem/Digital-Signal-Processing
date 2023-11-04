80d30b6c915f308f12d909e140a5a71d632f2ca8
Ali Tarek <98765988+AliTarekMohamed@users.noreply.github.com>
2023-11-05 01:50:18 +0200

    Add files via upload

--- Week4/Fouriertransform.py
+++ Week4/Fouriertransform.py
@@ -1,83 +1,114 @@
 from tkinter import *
-import tkinter as tk
-import tkinter.font as font
 from tkinter import filedialog
 import numpy as np
 import matplotlib.pyplot as plt
+import signalcompare as tst
 
+root = Tk()
+root.title("Week 4")
+root.geometry("500x400")
 
-dialog = Tk()
-dialog.title("Week 4")
-dialog.geometry("500x400")
-
-
-def Fouriertransform():
-    x_value = []
-    xof_n = []
-    xof_k = []
+def Fourier_transform():
+    xValues = []
+    yValues = []
+    xOfn = []
+    xOfk = []
     amplitude = []
     phase = []
-    segma_fundfreq = []
+    sigma = []
 
-    signal1 = filedialog.askopenfilename(
-        initialdir="Lab 2/input",
-        title="Signal 1",
+    signal = filedialog.askopenfilename(
+        initialdir="Lab 4/Test Cases",
+        title="Which Signal ?",
     )
-    file_label.config(text=f"Selected File: {signal1}")
+    chk = int(np.loadtxt(signal, skiprows = 1, max_rows = 1))
 
-    with open(signal1, "r") as f:
-        for i in range(3):
+    with open(signal, "r") as f:
+        for i in range(3): 
             next(f)
 
         for line in f:
             parts = line.strip().split()
-            x_value.append(float(parts[0]))
-            xof_n.append(float(parts[1]))
+            xValues.append(float(parts[0]))
+            yValues.append(float(parts[1]))
 
-    N = len(xof_n)
-    j = 1j
+    N = len(yValues)
+    
+    if (chk == 0):
+        xOfn = yValues
+        j = 1j
 
-    for k in range(0, N):
-        harmonic_value = 0
-        for n in range(0, N):
-            harmonic_value += xof_n[n]*np.exp((-j*2*np.pi*k*n)/N)
-            # xof_k.append(xof_n[n]*np.exp((-j*2*np.pi*k*n)/N))
-        xof_k.append(harmonic_value)
-        amplitude.append(np.sqrt(
-            np.power(np.real(harmonic_value), 2)+np.power(np.imag(harmonic_value), 2)))
-        phase.append(
-            np.arctan(np.imag(harmonic_value)/np.real(harmonic_value)))
+        for k in range (0, N):
+            harmonic_value = 0
+            for n in range (0, N):
+                harmonic_value += xOfn[n] * np.exp((-j * 2 * np.pi * k * n) / N)
+            xOfk.append(harmonic_value)
+            amplitude.append(np.sqrt(np.power(np.real(harmonic_value), 2) + np.power(np.imag(harmonic_value), 2)))
+            phase.append(np.arctan(float(np.imag(harmonic_value) / np.real(harmonic_value))))
 
-    Fs = int(freqtxt.get("1.0", END))
-    fundamentalfreq = 0
+        fund_freq = 0
+        fs = int(freq.get(1.0, "end"))
 
-    for i in range(0, len(amplitude)):
-        fundamentalfreq += (2*np.pi*Fs)/N
-        segma_fundfreq.append(fundamentalfreq)
+        for i in range(0, len(amplitude)):
+            fund_freq += (2 * np.pi * fs) / N
+            sigma.append(fund_freq)
 
-    figure, add = plt.subplots(2, 1, figsize=(6, 8))
+        # if(tst.SignalComapreAmplitude(yValues,amplitude) and tst.SignalComaprePhaseShift(yValues,phase)):
+        #     print ("Success")
+        
+        modWindow = Toplevel(root)
+        modWindow.title("Modification")
+        modWindow.geometry("500x400")
+        frame = Frame(modWindow)
+        lf1 = LabelFrame(frame, text="Modify Amplitude")
+        lf2 = LabelFrame(frame, text="Modify Phase Shift")
+        amp_mod = Text(lf1, width=20, height=2)
+        phase_mod = Text(lf2, width=20, height=2)
+        # update = Button(frame, text="Update", width="12", height="2", font="30", bg="white", fg="black", command=)
+        frame.pack()
+        lf1.pack()
+        lf2.pack()
+        amp_mod.pack()
+        phase_mod.pack()
+        # update.pack()
+        figure, DFT = plt.subplots(2, 1, figsize=(6, 8))
+        DFT[0].bar(sigma, amplitude)
+        DFT[0].set_title("Frequency / Amplitude")
+        DFT[1].bar(sigma, phase)
+        DFT[1].set_title("Frequency / Phase")
 
-    add[0].plot(segma_fundfreq, amplitude)
-    add[0].set_title("Frequency/Amplitude")
-    add[1].plot(segma_fundfreq, phase)
-    add[1].set_title("Frequency/Phase")
+    else:
+        real = []
+        img = []
+        j = 1j
+        
+        for i in range(N):
+            real.append(xValues[i] * np.cos(yValues[i]))
+            img.append(xValues[i] * np.sin(yValues[i]))
+            xOfk.append(real[i] + img[i] * j)
+
+        for n in range (0, N):
+            harmonic_value = 0
+            for k in range (0, N):
+                harmonic_value += (1 / N) * xOfk[k] * np.exp((j * 2 * np.pi * n * k) / N)
+            xOfn.append(harmonic_value)
+
+        # output = np.loadtxt("Lab 4\Test Cases\IDFT\Output_Signal_IDFT.txt", skiprows=3, delimiter=' ')
+        # tst_output = output[:, 1]
+        # if tst.SignalComapreAmplitude(xOfn, tst_output):
+        #     print("Success")
+
+        plt.plot(xOfn)
+        plt.xlabel("time")
+        plt.ylabel("amplitude")
+        plt.title("IDFT")
     plt.show()
-    print(xof_k)
-    print(amplitude)
-    print(phase)
 
+freq_label = Label(root, text="Enter the Sampling Frequency in HZ", pady=40)
+freq = Text(root, width=20, height=2)
+upload = Button(root, text="Upload", width=12, height=2, command=Fourier_transform)
 
-file_label = tk.Label(dialog, text="Selected File: ", pady=40)
-uploadbtn = Button(dialog, text="Upload", width=12,
-                   height=2, command=Fouriertransform)
-textfield_label = tk.Label(
-    dialog, text="Enter the Sampling Frequency in HZ", pady=40)
-freqtxt = Text(dialog, width=20, height=2)
-
-
-textfield_label.pack()
-freqtxt.pack()
-file_label.pack()
-uploadbtn.pack()
-
-dialog.mainloop()
+freq_label.pack()
+freq.pack()
+upload.pack()
+root.mainloop()
